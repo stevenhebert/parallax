95638f86feb30c4febe66df6ad2a0af41ace1dfc
steven <shebert2@cnm.edu>
2017-08-22 23:02:18 -0600

    test

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+
+			<meta charset="utf-8"/>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+			<!-- Optional theme -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+			<!-- Latest compiled and minified JavaScript -->
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+			<!-- from BCC form -->
+			<!-- jQuery Form, Additional Methods, Validate -->
+			<script type="text/javascript"
+					  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+			<script type="text/javascript"
+					  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+			<script type="text/javascript"
+					  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+			<!-- Your JavaScript Form Validator -->
+			<script src="js/form-validate.js"></script>
+			<!-- Google reCAPTCHA -->
+			<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+
+			<!-- Google Fonts -->
+			<link href="https://fonts.googleapis.com/css?family=Playball|Roboto:400,400i,700,700i" rel="stylesheet"/>
+
+			<!-- Font Awesome -->
+			<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+					rel="stylesheet">
+
+			<!-- custom stylesheet -->
+			<link rel="stylesheet" href="css/style.css"/>
+
+			<!-- HTML5 shim and Respond . js for IE8 support of HTML5 elements and media queries -->
+			<!-- WARNING: Respond . js doesn't work if you view the page via file:// -->
+			<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<![endif]-->
+
+			<title>steven hebert</title>
+
+		</head>
+
+		<body>
+			<div class="stars">
+						<div class="twinkling">
+
+					<input id="debug" type="checkbox">
+					<label for="debug">Debug: Transition to </label>
+
+					<div class="wrapper">
+						<div class="slides">
+							<div class="slide">
+								<div class="title">Pure CSS z-scrolling</div>
+							</div>
+							<div class="slide">
+								<div class="title">Slide 1</div>
+							</div>
+							<div class="slide">
+								<div class="title">Slide 2</div>
+							</div>
+							<div class="slide">
+								<div class="title">Slide 3</div>
+							</div>
+							<div class="slide">
+								<div class="title">Slide 4</div>
+							</div>
+							<div class="slide">
+								<div class="title">Slide 5</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</body>
